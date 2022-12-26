77cc0d9dbc017e1ce6f38a3a8ab7ea10f978316c
adhithan-m <116782079+adhithan-m@users.noreply.github.com>
2022-12-26 16:30:26 +0530

    table.

---
+++ table/table.js
@@ -0,0 +1,47 @@
+//#table
+
+/*a=document.createElement("Button")
+a.setAttribute("onclick","start();");
+a.innerHTML="click Me"
+document.body.appendChild(a);
+
+function start(){
+   b=document.createElement("Button");
+   b.setAttribute("onclick","multiple();"); 
+   b.innerHTML="start";
+   document.body.appendChild(b)
+   
+   c=document.createElement("Button");
+    c.setAttribute("onclick","run();");
+    c.innerHTML="run";
+    document.body.appendChild(c)
+}
+
+function multiple(){
+    var x = document.createElement("input");
+    x.setAttribute("type", "text");
+    x.setAttribute("placeholder", "Enter the number:");
+    x.setAttribute("id","n1");
+    document.body.appendChild(x);
+	
+    var y = document.createElement("input");
+    y.setAttribute("type", "text");
+    y.setAttribute("placeholder", "Enter the range:");
+    y.setAttribute("id","n2");
+    document.body.appendChild(y);
+	
+	
+ }
+ 
+function run(){
+num1 = Number(document.getElementById("n1").value);
+num2 = Number(document.getElementById("n2").value);
+var  i=1;
+while(i<=num2)
+{
+document.write(i+  "  X  "  +num1+  "  =  "  +i*num1+ "<br>");
+i++;
+}
+}*/
+
+
